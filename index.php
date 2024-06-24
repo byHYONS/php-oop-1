559766429e859b2a2a5050d359c0503d08aa0f03
byHYONS <byhyons@gmail.com>
2024-06-24 18:22:38 +0200

    feat: declared instance variables

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
 class Movie {
-    
-}
+   private string $nameFilm;
+   private string $description;
+   private int $vote;
+
+   function __construct(int $vote)
+   {
+    $this -> vote = $vote;
+   }
+
+    public function getNameFilm (string $_name){
+        return $this -> nome = $_name;
+    }
+};
